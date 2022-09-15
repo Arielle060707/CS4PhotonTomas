fffec23f71e4f484ec789612d2251e8a2563e4f2
Amaris Laelle A. Tomas <90588185+Arielle060707@users.noreply.github.com>
2022-09-15 23:36:09 +0800

    Update Ex02PhotonTomas.java

--- Ex02PhotonTomas.java
+++ Ex02PhotonTomas.java
@@ -1,63 +1,91 @@
 public class Ex02PhotonTomas {
-
+    private static String userPlay; 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int random = (int) Math.floor(Math.random()*10) + 1;
-        
-        System.out.println("Welcome to Higher or Lower! What would you like to do?");
-        System.out.println("Start game");
-        System.out.println("Change settings");
-        System.out.println("End application");
-        
-        String choiceOne = "Start game";
-        String choiceTwo = "Change settings";
-        String choiceThree = "End application";
+    int minNum = 1;
+        int maxNum = 10;
+        int random = (int) Math.floor(Math.random()*maxNum) + minNum;
         int guessAtmpts = 3;
-        boolean response = false;
-        
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-        
-        if (userInput.equalsIgnoreCase(choiceOne))
+        int numGuess;
+        char userPlay = 0;
+              
+        do
         {
-            do 
-            {  
-                while(guessAtmpts > 0)
+            System.out.println("Welcome to Higher or Lower! What would you like to do?" 
+            + "\n-Start game" 
+            + "\n-Change settings" 
+            + "\n-End application");
+        
+            Scanner sc = new Scanner(System.in);
+            String userInput = sc.nextLine();
+            
+            if(userInput.equalsIgnoreCase("Start game"))
+            {
+                int i = guessAtmpts;
+                
+                while(i >= 0)
                 {
-                    int userGuess = sc.nextInt();
-                    System.out.println("You have " + guessAtmpts + " left. What is your guess?");
-                
-                    guessAtmpts--;
-                
-                    if (userGuess < random)
+                    System.out.println("You have " + i + " guess(es) left. What is your guess?");
+                    numGuess = sc.nextInt();
+                    
+                    if(numGuess == random)
                     {
-                        System.out.println("Guess higher! You have " + guessAtmpts + " guess(es) left. What is your guess?");
+                       System.out.println("You got it!");
                     }
-                    else if (userGuess > random)
+                    else if (numGuess > random)
                     {
-                         System.out.println("Guess lower! You have " + guessAtmpts + " guess(es) left. What is your guess?");
+                        i--;
+                        System.out.println("Guess lower! You have " + i + " guess(es) left. What is your guess?");
+                        numGuess = sc.nextInt();
+                        
+                        if (numGuess == random)
+                        {
+                            System.out.println("You got it!");
+                        }    
                     }
-                    else
+                    else if (numGuess < random)
                     {
-                        System.out.println("You got it!");
+                        i--;
+                        System.out.println("Guess higher! You have " + i + " guess(es) left. What is your guess?");
+                        numGuess = sc.nextInt();
+                        
+                        if (numGuess == random)
+                        {
+                            System.out.println("You got it!");
+                        }    
                     }
-                }
-                System.out.println("Play again (y/n)?");
+                    
+                    break;
+                } 
                 
-            }while (!response);
-        }  
+                System.out.println("Play again (y/n)? ");
+                userPlay = sc.next().charAt(0);
+            }    
+            else if (userInput.equalsIgnoreCase("Change settings"))
+            {
+                System.out.println("What is the lower limit of the number?" 
+                + "\nWhat is the upper limit of the number?" 
+                + "\nHow many guesses are allowed?");
+                
+                System.out.println("Lower limit: ");
+                minNum = sc.nextInt();
+                System.out.println("Upper limit: ");
+                maxNum = sc.nextInt();
+                System.out.println("Guesses: ");
+                guessAtmpts = sc.nextInt();
+            }
+            else if (userInput.equalsIgnoreCase("End application"))
+            {
+                System.out.println("\nThank you for playing!");
+                break; //ending the loop quickly
+            }    
+            else 
+            {
+                System.out.println("You lost...");
+            }    
         
-        /* (userInput.equalsIgnoreCase(choiceTwo))
-        {
-            what on earth do i do????
-        }
-        */
-        if(userInput.equalsIgnoreCase(choiceThree))
-        {
-            System.out.println("\nThank you for playing!");
-        }
+        }while(userPlay == 'y');
     }
-    
 }
